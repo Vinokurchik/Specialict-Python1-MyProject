44ac36cfc92b4766d56f19aa9a18bb20ecbef7b3
i <110337497+Vinokurchik@users.noreply.github.com>
2022-08-07 16:38:06 +0300

    Create 02_max2.py

---
+++ 02_max2.py
@@ -0,0 +1,15 @@
+# Напишите функцию, возвращающую наибольшее из двух чисел
+
+def max2(n1, n2):
+    if n1 - n2 > 0:
+        m = n1
+    elif n2 - n1 >0:
+        m = n2
+    return m
+
+
+print(max2(5, 6))
+print(max2(-10, -12))
+print(max2(2.5, 2.6))
+print(max2(-2.5, 0))
+print(max2(0, -2.5))
