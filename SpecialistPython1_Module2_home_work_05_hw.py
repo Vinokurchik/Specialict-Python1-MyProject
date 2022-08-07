1119ca0ec8add448a514bfa4bf3aa63932d8ad32
i <110337497+Vinokurchik@users.noreply.github.com>
2022-08-07 10:39:36 +0300

    Update SpecialistPython1_Module2_home_work_05_hw.py

--- SpecialistPython1_Module2_home_work_05_hw.py
+++ SpecialistPython1_Module2_home_work_05_hw.py
@@ -13,15 +13,11 @@ a = int(input("a: "))
 b = int(input("b: "))
 c = int(input("c: "))
 
-if a <= b and b <=c:
-    print(a, b, c)
-elif a <= c and c <= b:
-    print(a, c, b)
-elif b <= a and a <= c:
-    print(b, a, c)
-elif b <= c and c <= a:
-    print(b, c, a)
-elif c <= a and a <= b:
-    print(c, a, b)
-elif c <= b and b <= a:
-    print(c, b, a)
+if b < a:
+    a, b = b, a
+if c < a:
+    c, a = a, c
+if c < b:
+    c, b = b, c
+
+print(a, b, c)
